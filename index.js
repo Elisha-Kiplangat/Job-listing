30f19f79832550dda6aaab61f2465b5886d4b565
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-05-31 11:02:14 +0300

    update

--- index.js
+++ index.js
@@ -1,152 +1,74 @@
-// Start Variables
-let listsContainer = document.querySelector(".lists");
-let filterList = document.querySelector(".filter-list");
-let filterListContainer = document.querySelector(".filter-list .filters");
-let clearButtonFilters = document.querySelector("button");
-// End Variables
-// Start Fetching
-async function getData(condition) {
-  let data = await (await fetch("data.json")).json();
-  if (condition) {
-    let newData = [];
-    let found;
-    for (let i = 0; i < data.length; i++) {
-      found = true;
-      for (let j = 1; j < filterListContainer.attributes.length; j++) {
-        let dataName = filterListContainer.attributes[j].name
-          .match(/-\w+-/gi)
-          .join("")
-          .slice(1, -1);
+const filtersSelected = new Set();
+const filters = document.querySelector('.filter-cover');
+const clear = document.querySelector('.clear');
 
-        if (dataName == "languages") {
-          let language = filterListContainer.attributes[j].value;
+const bgImg = document.getElementById('bg-img');
 
-          if (data[i][dataName].indexOf(language) == -1) {
-            found = false;
-          }
-        } else if (dataName == "tools") {
-          let tool = filterListContainer.attributes[j].value;
-
-          if (data[i][dataName].indexOf(tool) == -1) {
-            found = false;
-          }
-        } else if (dataName == "role") {
-          let role = filterListContainer.attributes[j].value;
-
-          if (data[i][dataName] != role) {
-            found = false;
-          }
-        } else {
-          let level = filterListContainer.attributes[j].value;
-
-          if (data[i][dataName] != level) {
-            found = false;
-          }
-        }
-      }
-
-      if (found) {
-        newData.push(data[i]);
-      }
-    }
-    createData(newData);
-  } else {
-    createData(data);
-  }
+if(window.innerWidth <= 540 ) {
+    let bg = bgImg.src.split('/') ;
+    console.log(bg , bg.length );
+    bg[bg.length-1] = 'bg-header-mobile.svg' ;
+    bgImg.src = bg.join('/');
 }
-// End Fetching
-// Start Creating Data
-function createData(array) {
-  listsContainer.innerHTML = "";
-  for (let i = 0; i < array.length; i++) {
-    let list = document.createElement("div");
-    list.classList.add("list");
-    if (array[i].new && array[i].featured) {
-      list.classList.add("on-the-top");
-    }
 
-    let description = document.createElement("div");
-    description.classList.add("description");
 
-    let img = document.createElement("img");
-    img.src = array[i].logo;
-    img.alt = array[i].company.toLowerCase() + " image";
-    description.appendChild(img);
+const filterParent = filters.parentElement;
 
-    let details = document.createElement("div");
-    details.classList.add("details");
+let data, main = document.querySelector('main'), realJobList = [];
 
-    let company = document.createElement("div");
-    company.classList.add("company");
-    company.appendChild(document.createTextNode(array[i].company));
+fetch('data.json')
+    .then(data => data.json())
+    .then(res => {
+        data = res;
+        createList(data);
+    });
 
-    let spanNew = document.createElement("span");
-    spanNew.classList.add("new");
-    spanNew.appendChild(document.createTextNode("new"));
-    let spanFeatured = document.createElement("span");
-    spanFeatured.appendChild(document.createTextNode("featured"));
-    spanFeatured.classList.add("featured");
-    if (array[i].new && array[i].featured) {
-      let p = document.createElement("p");
-      p.classList.add("top");
-      p.appendChild(spanNew);
-      p.appendChild(spanFeatured);
-      company.appendChild(p);
-    } else if (array[i].new) {
-      company.appendChild(spanNew);
-    } else if (array[i].featured) {
-      company.appendChild(spanFeatured);
-    }
-    details.appendChild(company);
+function createList(data) {
 
-    let h3 = document.createElement("h3");
-    h3.classList.add("position");
-    h3.appendChild(document.createTextNode(array[i].position));
-    details.appendChild(h3);
+    // filterParent.classList.add('none');
 
-    let time = document.createElement("div");
-    time.classList.add("time");
-    let spanPostedAt = document.createElement("span");
-    spanPostedAt.classList.add("posted-at");
-    spanPostedAt.appendChild(document.createTextNode(array[i].postedAt));
-    time.appendChild(spanPostedAt);
-    let spanContract = document.createElement("span");
-    spanContract.classList.add("contract");
-    spanContract.appendChild(document.createTextNode(array[i].contract));
-    time.appendChild(spanContract);
-    let spanLocation = document.createElement("span");
-    spanLocation.classList.add("location");
-    spanLocation.appendChild(document.createTextNode(array[i].location));
-    time.appendChild(spanLocation);
+    let docf = document.createDocumentFragment();
+    data.forEach(el => {
+        let listing = document.createElement('div');
+        listing.classList.add('listing');
 
-    details.appendChild(time);
-    description.appendChild(details);
-    list.appendChild(description);
+        let listFrag = document.createDocumentFragment();
+        listing.companyName = el.company
+        listing.data_filters = new Set([...el.languages, ...el.tools, el.role, el.level]);
+        listing.data_filters.forEach(l => {
+            let li = document.createElement('li');
+            li.classList.add('job-filter');
+            li.textContent = l;
+            li.setAttribute('data-filter', l);
+            listFrag.appendChild(li);
+        })
 
-    let stats = document.createElement("div");
-    stats.classList.add("stats");
-    }
-}
-let spanRole = document.createElement("span");
-spanRole.setAttribute(`data-role-${array[i].role}`, array[i].role);
-list.setAttribute(`data-role-${array[i].role}`, array[i].role);
-spanRole.appendChild(document.createTextNode(array[i].role));
-stats.appendChild(spanRole);
-let spanLevel = document.createElement("span");
-spanLevel.setAttribute(`data-level-${array[i].level}`, array[i].level);
-list.setAttribute(`data-level-${array[i].level}`, array[i].level);
-spanLevel.appendChild(document.createTextNode(array[i].level));
-stats.appendChild(spanLevel);
-for (let j = 0; j < array[i].languages.length; j++) {
-  let spanLanguage = document.createElement("span");
-  spanLanguage.setAttribute(
-    `data-languages-${array[i].languages[j]}`,
-    array[i].languages[j]
-  );
-  list.setAttribute(
-    `data-languages-${array[i].languages[j]}`,
-    array[i].languages[j]
-  );
-  spanLanguage.appendChild(document.createTextNode(array[i].languages[j]));
-  stats.appendChild(spanLanguage);
-}
+        listing.innerHTML = `
+        <div class="left">
+            <div class="listing-img">
+                <img src="${el.logo}" alt="">
+            </div>
+            <div class="company-details">
+                <div class="company-nametags">
+                    <h4 class="company-name">${el.company}</h4>
+                    <div class="company-tags">
+                        <p class="company-tag-new" style="display:${ el.new ? "block" : 'none'}" >NEW</p>
+                        <p class="company-tag-ftrd" style="display:${ el.featured ? "block" : 'none'}" >FEATURED</p>
+                    </div>
+                </div>
+                <h2 class="job-role">${el.position}</h2>
+                <div class="job-min-details">
+                    <p class="job-time">${el.postedAt}</p>
+                    <p class="contOrFull">${el.contract}</p>
+                    <p class="job-location">${el.location}</p>
+                </div>
+            </div>
+        </div>
+        <ul class="job-filters">
+        </ul>
+        ` ;
+
+        listing.querySelector('.job-filters').append(listFrag);
+        realJobList.push(listing);
+        docf.append(listing);
+    });
